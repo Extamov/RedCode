56d9d779e1dcb580e2b9752102396ff418054451
Extamov <extamov@gmail.com>
2024-01-21 01:26:48 +0200

    Fixed a bug that came from tzofar's new ping msg

--- redcode/providers/tzofar.py
+++ redcode/providers/tzofar.py
@@ -4,6 +4,9 @@ from json import loads
 def parse_func(message):
     data = loads(message)
 
+    if "type" in data and data["type"] == "ANDROID_KEEPALIVE":
+        return {"title": "", "alerts": []}
+
     if "data" not in data or "cities" not in data["data"] or "notificationId" not in data["data"]:
         raise ValueError()
 
