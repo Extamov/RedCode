9edc3d7ab9c5cf51185b1c48b38b7ada15f53af4
Extamov <extamov@gmail.com>
2024-09-28 22:07:09 +0300

    Fixed filtering ping tests from tzofar again, they changed the format after few days lol

--- redcode/providers/tzofar.py
+++ redcode/providers/tzofar.py
@@ -4,7 +4,7 @@ from json import loads
 def parse_func(message):
     data = loads(message)
 
-    if "type" in data and data["type"] in ["ANDROID_KEEPALIVE", "LATENCY_TEST_WS"]:
+    if "type" in data and data["type"] in ["ANDROID_KEEPALIVE", "LATENCY_TEST_WS", "TEST_LATANCY_WS"]:
         return {"title": "", "alerts": []}
 
     if "data" not in data or "cities" not in data["data"] or "notificationId" not in data["data"]:
