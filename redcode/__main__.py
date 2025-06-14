0a041f4efb3763cc02fbb5a4af7c5347abd9a985
Extamov <extamov@gmail.com>
2025-06-14 06:17:40 +0300

    Updated user agent

--- redcode/__main__.py
+++ redcode/__main__.py
@@ -8,7 +8,7 @@ from .providers import PROVIDERS
 
 def create_session():
     sess = ClientSession(timeout=ClientTimeout(total=20), connector=TCPConnector(ttl_dns_cache=300), headers={
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
     })
     return sess
 
